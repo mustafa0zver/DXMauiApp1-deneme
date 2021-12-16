1bd907d749575f8c9a70328aedc0b932acc92a91
Mustafa ÖZVER <95069545+mustafa0zver@users.noreply.github.com>
2021-12-16 13:36:37 +0300

    Proje dosyası ekle.

---
+++ DXMauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+using System.Collections.ObjectModel;
+
+namespace DXMauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Setup();
+        }
+        public void Setup()
+        {
+            
+        }
+
+    }
+
+
+
+}
---
+++ DXMauiApp1/MauiProgram.cs
@@ -0,0 +1,52 @@
+﻿using DevExpress.Maui.Charts;
+using DevExpress.Maui.CollectionView;
+using DevExpress.Maui.DataGrid;
+using DevExpress.Maui.Editors;
+using DevExpress.Maui.Navigation;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace DXMauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    handlers.AddHandler<DXCollectionView, DXCollectionViewHandler>();
+                    handlers.AddHandler<ChartView, ChartViewHandler>();
+                    handlers.AddHandler<PieChartView, PieChartViewHandler>();
+                    handlers.AddHandler<DataGridView, DataGridViewHandler>();
+                    handlers.AddHandler<SimpleButton, SimpleButtonHandler>();
+                    handlers.AddHandler<TextEdit, TextEditHandler>();
+                    handlers.AddHandler<CheckEdit, CheckEditHandler>();
+                    handlers.AddHandler<NumericEdit, NumericEditHandler>();
+                    handlers.AddHandler<PasswordEdit, PasswordEditHandler>();
+                    handlers.AddHandler<MultilineEdit, MultilineEditHandler>();
+                    handlers.AddHandler<ComboBoxEdit, ComboBoxEditHandler>();
+                    handlers.AddHandler<DateEdit, DateEditHandler>();
+                    handlers.AddHandler<TimeEdit, TimeEditHandler>();
+                    handlers.AddHandler<AutoCompleteEdit, AutoCompleteEditHandler>();
+                    handlers.AddHandler<DrawerView, DrawerViewHandler>();
+                    handlers.AddHandler<TabView, TabViewHandler>();
+                    handlers.AddHandler<DrawerPage, DrawerPageHandler>();
+                    handlers.AddHandler<TabPage, TabPageHandler>();
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("univia-pro-regular.ttf", "Univia-Pro");
+                    fonts.AddFont("roboto-bold.ttf", "Roboto-Bold");
+                    fonts.AddFont("roboto-regular.ttf", "Roboto");
+                });
+
+            return builder.Build();
+        }
+    }
+}
